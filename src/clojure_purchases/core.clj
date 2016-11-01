451279751c9b31959fa40683a4a756c9fff768a2
michaelplott <mike.plott80@gmail.com>
2016-11-01 15:39:27 -0400

    had it working but ran into an error trying to fix

--- src/clojure_purchases/core.clj
+++ src/clojure_purchases/core.clj
@@ -1,10 +1,26 @@
 (ns clojure-purchases.core
-  (:require [clojure.string :as str])
+  (:require [clojure.string :as str]
+            [compojure.core :as c]
+            [ring.adapter.jetty :as j]
+            [hiccup.core :as h])
   (:gen-class))
 
-(def file-name "purchases.csv")
+;        category (read-line)
+;        purchases (filter (fn [line]
+;                            (= (get line "category")
+;                               category
+;                    purchases)
+;        purchases (pr-str purchases)]
+;    (println purchases)
+;    (spit "filtered_purchases.edn" purchases)))
 
-(defn -main [& args]
+
+(def file-name "purchases.csv")
+;(def prompt (println "Enter a category"
+;                 (println (def choice read-line))))
+;(def choice (read-line))
+
+(defn read-purchases []
   (let [purchases (str/split-lines (slurp file-name))
         purchases (map (fn [line]
                          (str/split line #","))
@@ -13,15 +29,42 @@
         purchases (rest purchases)
         purchases (map (fn [line]
                          (zipmap header line))
-                    purchases)
-        purchases (filter (fn [line]
-                            (= (get line "category")
-                               "Furniture"))
-                    purchases)
-        purchases (pr-str purchases)]
-    (spit "filtered_purchases.edn" purchases)))
-                   
-                    
-        
-   
+                    purchases)]
+    purchases))
+;customer_id,date,credit_card,cvv,category
+(defn purchases-html [category]
+  (let [purchases (read-purchases)
+        purchases (filter (fn [purchase]
+                            (or (nil? category)
+                                (= category (get purchase "category"))))
+                    purchases)]
+    [:table {:width 800 :border 1}
+     [:th "Customer id"]
+     [:th "Purchase date"]
+     [:th "CC Number"]
+     [:th "CVV Number"]
+     [:th "Purchase Category"]
+     (map fn [purchase] purchases
+             [:tr
+              [:td (str
+                     (get purchase "customer_id"))]
+              [:td (str
+                     (get purchase "date"))]
+              [:td (str
+                     (get purchase "credit_card"))]
+              [:td (str
+                     (get purchase "cvv"))]
+              [:td (str
+                     (get purchase "category"))]]
+             purchases)]))
 
+
+
+(c/defroutes app
+  (c/GET "/" []
+    (h/html [:html [:body (purchases-html nil)]]))
+  (c/GET "/:category" [category]
+    (h/html [:html [:body (purchases-html category)]])))
+
+(defn -main [& args]
+  (j/run-jetty app {:port 3000}))
